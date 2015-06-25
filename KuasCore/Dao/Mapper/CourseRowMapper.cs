8249380797a840ad26d942661c423a5f585b4547
1101137236 <w2497imt@gmail.com>
2015-06-25 20:11:45 +0800

    ugly grid

---
+++ KuasCore/Dao/Mapper/CourseRowMapper.cs
@@ -0,0 +1,30 @@
+﻿using KuasCore.Models;
+using Spring.Data.Generic;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCore.Dao.Mapper
+{
+    class CourseRowMapper : IRowMapper<Course>
+    {
+        public Course MapRow(IDataReader dataReader, int rowNum)
+        {
+            Course target = new Course();
+
+            target.Id = dataReader.GetString(dataReader.GetOrdinal("Course_ID"));
+            target.Name = dataReader.GetString(dataReader.GetOrdinal("Course_Name"));
+            target.Description = dataReader.GetString(dataReader.GetOrdinal("Course_Description"));
+            target.Tel = dataReader.GetString(dataReader.GetOrdinal("Course_Tel"));
+            target.Start = dataReader.GetString(dataReader.GetOrdinal("Course_Start"));
+            target.End = dataReader.GetString(dataReader.GetOrdinal("Course_End"));
+            target.Here = dataReader.GetString(dataReader.GetOrdinal("Course_Here"));
+            target.Rest = dataReader.GetString(dataReader.GetOrdinal("Course_Rest"));
+
+            return target;
+        }
+    }
+}
---
+++ KuasCore/Models/Course.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCore.Models
+{
+    public class Course
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Tel { get; set; }
+
+        public string Start { get; set; }
+
+        public string End { get; set; }
+
+        public string Here { get; set; }
+
+        public string Rest { get; set; }
+
+        public override string ToString()
+        {
+            return "Course: Id = " + Id + ", Name = " + Name + ".";
+        }
+    }
+}
